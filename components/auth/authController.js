f114b349313470298ab16de4736e91704d04521d
= <ryanchenkie@gmail.com>
2015-08-11 21:59:07 -0600

    Indenting

--- components/auth/authController.js
+++ components/auth/authController.js
@@ -15,51 +15,51 @@
 
 		function createUser() {
 
-	    // If there is already a user logged in,
-	    // log them out before proceeding
-	    Auth.$unauth();
+			// If there is already a user logged in,
+			// log them out before proceeding
+			Auth.$unauth();
 
-	    Auth.$createUser({
-	    	email: vm.email,
-	      password: vm.password
-	    }).then(function(userData) {
-	      saveUser(userData);		        
-	      login();
-	    }).catch(function(error) {
-	      vm.error = error;
-	    });
+			Auth.$createUser({
+				email: vm.email,
+				password: vm.password
+			}).then(function(userData) {
+				saveUser(userData);
+				login():
+			}).catch(function(error) {
+				vm.error = error;
+			});
 		}
 
 		function saveUser(userData) {
 
 			var user = User.newUserRef(userData);
 			user.username = vm.username;
-		  user.email = vm.email;
+			user.email = vm.email;
 
-      user.$save().then(function(success) {
-      	vm.username = null;
-      	vm.email = null;
-      	vm.password = null;	
-  		$state.go('status');
-      }, function(error) {
-      	console.log("there was an error! " + error);
-      });
+			user.$save().then(function(success) {
+				vm.username = null;
+				vm.email = null;
+				vm.password = null;
+
+				$state.go('status');
+			}, function(error) {
+				console.log("there was an error! " + error);
 		}
 
 		function login() {
 
-	    Auth.$authWithPassword({
-	    	email: vm.email,
-	    	password: vm.password
-	    }).then(function(data) {
-	    	vm.email = null;
-	      vm.password = null;
-	      $state.go('status');
-	    }).catch(function(error) {
-	    	console.log(error);
-	    });
-		}	
+			Auth.$authWithPassword({
 
+				email: vm.email,
+				password: vm.password
+			}).then(function(data) {
+				vm.email = null;
+				vm.password = null;
+				$state.go('status');
+			}).catch(function(error) {
+				console.log(error);
+			});
+		}
 	}
 
 })();
